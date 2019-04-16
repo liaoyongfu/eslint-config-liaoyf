8888cd3ac5b11e49a4336f0c60fc092d5b749f86
liaoyongfu <893753543@qq.com>
2019-04-16 15:35:55 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# eslint-plugin-vv
+
+前端 React 项目 EsLint 配置文件。主要使用：
+
+- [`airbnb 代码风格指南`](https://github.com/airbnb/javascript)
+- [`prettier`](https://prettier.io)：代码格式化
+- [`eslint-config-compat`](https://github.com/amilajack/eslint-plugin-compat#readme)：检测兼容性代码
+
+## 覆盖规则列表
+
+具体覆盖规则请查看 index.js 文件。
+
+## 安装
+
+````
+$ yarn add eslint-plugin-vv eslint -D
+// or
+$ npm install --dev eslint-plugin-vv
+````
+
+## 使用
+
+在根目录中新建`.eslintrc.js`文件：
+
+````
+module.exports = {
+    extends: ["vv"],
+    rules: {
+        // 进行规则覆盖
+    }
+};
+````
+
+## IDE 开启 eslint 检测
+
+### Webstrom
+
+1. 在`File -> Setting -> Languages & Frameworks -> Javascript -> Code Quality Tools -> Eslint`中开启 Eslint 检测。
+2. 在`File -> Setting -> Keymap -> Plug-ins -> Javascript Support -> Fixed Eslint Problems`中右键点击`Add keyboard shortcut`，使用`Ctrl + E`覆盖默认快捷键。
+
+### VSCode
+
+[配置教程](https://segmentfault.com/a/1190000009077086)
---
+++ index.js
@@ -0,0 +1,51 @@
+module.exports = {
+    "extends": [
+        "airbnb",
+        "plugin:prettier/recommended",
+        'plugin:compat/recommended'
+    ],
+    env: {
+        browser: true,
+        node: true,
+        es6: true,
+        mocha: true,
+        jest: true,
+        jasmine: true
+    },
+    "parser": "babel-eslint",
+    "rules": {
+        "prettier/prettier": ['error', {
+            // 使用单引号
+            singleQuote: true,
+            // 指定每个缩进级别为 4 个空格数
+            tabWidth: 4
+        }],
+        // 缩进使用 4 个空格
+        "indent": ['error', 4],
+        // 仅使用.js为jsx代码的文件扩展名
+        "react/jsx-filename-extension": ['error', { "extensions": [".js"] }],
+        // 均使用领取一行的 jsx 写法
+        "react/jsx-wrap-multilines": ['error', {
+            "declaration": "parens-new-line",
+            "assignment": "parens-new-line",
+            "return": "parens-new-line",
+            "arrow": "parens-new-line",
+            "condition": "parens-new-line",
+            "logical": "parens-new-line",
+            "prop": "parens-new-line"
+        }],
+        // 禁止导入无关的模块
+        "import/no-extraneous-dependencies": ['error', {
+            "packageDir": path.join(__dirname, 'src'),
+            "devDependencies": ['**/tests/**.js', '/mock/**/**.js', '**/**.test.js']
+        }],
+        // 取消“非交互式元件不允许交互”
+        'jsx-a11y/no-noninteractive-element-interactions': 'off',
+        // click 事件不必伴随 key 键盘事件
+        'jsx-a11y/click-events-have-key-events': 'off',
+        // 取消“静态元素没有交互”
+        'jsx-a11y/no-static-element-interactions': 'off',
+        // <a>具有有效 href 属性
+        'jsx-a11y/anchor-is-valid': 'off',
+    }
+};
